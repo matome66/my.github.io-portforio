5995158ba85ee82ab6d61f720b6bc157aabb653b
matome66 <86086061+matome66@users.noreply.github.com>
2021-07-14 18:18:35 +0900

    Update scprit.js

--- scprit.js
+++ scprit.js
@@ -1,20 +1,9 @@
-jQuery(function ($) {  
-  //タイトルがクリックされたら
-  $(".js-accordion-title").on('click', function () {
-    //クリックしたjs-accordion-title以外の全てのopenを取る
-    $(".js-accordion-title").not(this).removeClass("open");
-    //クリックされたjs-accordion-title以外のcontentを閉じる
-    $(".js-accordion-title").not(this).next().slideUp(300);
-    //thisにopenクラスを付与
-    $(this).toggleClass("open");
-    //thisのcontentを展開、開いていれば閉じる
-    $(this).next().slideToggle(300);
-  });
-  
-  /* 下記でもOK
-   $(".js-accordion-title").on('click', function () {
-     $(".js-accordion-title").not(this).removeClass("open").next().slideUp(300);
-     $(this).toggleClass("open").next().slideToggle(300);
-   });
-   */
+jQuery(function ($) {
+$('.js-accordion-title').on('click', function () {
+  /*クリックでコンテンツを開閉*/
+  $(this).next().slideToggle(200);
+  /*矢印の向きを変更*/
+  $(this).toggleClass('open', 200);
+});
+
 });
