87111ba2cca366d88c118a8e1829eb5ec3c8551a
matome66 <86086061+matome66@users.noreply.github.com>
2021-07-14 15:59:30 +0900

    Update scprit.js

--- scprit.js
+++ scprit.js
@@ -1,3 +1,9 @@
-$('fa-youtube').animate({
-  'font-size':'400px'
-},1000);
+jQuery(function ($){
+  $(".anser").css("display","none");
+  $(".question").click(function () {
+$(".question").not(this).removeClass("open");
+$(".question").not(this).next().slideUp(300);
+    $(this).toggleClass("open");
+    $(this).next().slideToggle(300);
+    });
+});
