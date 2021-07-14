b35e14c0c28bd02f7f4c0534f2da77d5142cbd77
matome66 <86086061+matome66@users.noreply.github.com>
2021-07-14 18:16:46 +0900

    Update scprit.js

--- scprit.js
+++ scprit.js
@@ -1,9 +1,11 @@
-jQuery(function ($) {
-$('.js-accordion-title').on('click', function () {
-  /*クリックでコンテンツを開閉*/
-  $(this).next().slideToggle(200);
-  /*矢印の向きを変更*/
-  $(this).toggleClass('open', 200);
-});
-
+$(function () {
+  // 最初のコンテンツ以外は非表示
+  $(".accordion-content:not(:first-of-type)").css("display", "none");
+  // 矢印の向きを変えておく
+  $(".js-accordion-title:first-of-type").addClass("open");
+ 
+  $(".js-accordion-title").click(function () {
+    $(".open").not(this).removeClass("open").next().slideUp(300);
+    $(this).toggleClass("open").next().slideToggle(300);
+  });
 });
